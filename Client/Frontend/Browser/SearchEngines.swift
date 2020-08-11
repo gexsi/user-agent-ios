f949f38bb681cfc0b70682db92e51ccce464eae0
pkirakosyan <66566816+pkirakosyan@users.noreply.github.com>
2020-08-11 13:10:43 +0400

    GEX-32: Automate searchEngine, resetDisabledEngineNamesIfNeeded. (#4)
    * Automated searchEngine, resetDisabledEngineNamesIfNeeded.
    
    * Small change.

--- Client/Frontend/Browser/SearchEngines.swift
+++ Client/Frontend/Browser/SearchEngines.swift
@@ -156,7 +156,11 @@ class SearchEngines {
     }
 
     fileprivate func resetDisabledEngineNamesIfNeeded() {
-        let defaultEnabledEnginesIDs = ["google-b-m", "google-b-1-m", "bing", "ddg", "gexsi"]
+        var defaultEnabledEnginesIDs = ["google-b-m", "google-b-1-m", "bing", "ddg"]
+        let engineName = Features.Search.defaultEngineName.trimmingCharacters(in: .whitespacesAndNewlines)
+        if !engineName.isEmpty && !defaultEnabledEnginesIDs.contains(engineName) {
+            defaultEnabledEnginesIDs.append(Features.Search.defaultEngineName.lowercased())
+        }
         let reset = self.prefs.boolForKey(ResetDisabledEngineNames)
         if reset == nil || !reset! {
             var disabledNames = [String]()
